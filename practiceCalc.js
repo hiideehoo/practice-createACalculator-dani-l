bb937565d57e928f0c6119ffc6dca710693d4b9c
hiideehoo <hiideeho@gmail.com>
2025-09-12 20:34:44 -0500

    submission no. 2

--- practiceCalc.js
+++ practiceCalc.js
@@ -11,7 +11,7 @@ let calculator = {
     smallToBig(arr) {
         return [Math.min(...arr), Math.max(...arr)]
     },
-    random(num) {
+    randomNum(num) { // added 'Num' to not confuse with .random Math method
         return (Math.floor((Math.random() * num) + 1))
     },
     round(num) {
@@ -25,5 +25,5 @@ console.log(calculator.absoluteValue(-45.67)); // 45.67
 console.log(calculator.power(5, 3)); // 125
 console.log(calculator.square(144)); // 12
 console.log(calculator.smallToBig([3, 78, -12, 0.5, 27])); // [ -12, 78 ]
-console.log(calculator.random(50)); // 1-50 // I got 31
+console.log(calculator.randomNum(50)); // 1-50 // I got 31
 console.log(calculator.round(23.67891)); // 23.68
